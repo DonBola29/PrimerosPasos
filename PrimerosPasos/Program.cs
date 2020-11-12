65431167ec60a1dd5c1764219ebc01e49a73e701
BOLIVAR RICARDO RAMIREZ LAGOS <bolivar.ramirez@pucese.edu.ec>
2020-11-12 12:37:34 -0500

    Funciones que devuelven valores y funciones que no devuelven valores

--- PrimerosPasos/Program.cs
+++ PrimerosPasos/Program.cs
@@ -48,7 +48,42 @@ namespace PrimerosPasos
             bool sw;
             sw = (a+50) > b;
             Console.WriteLine("El valor del booleano sw: {0}", sw);
+
+            //PILASRES FUNDAMENTALES DE LA PROGRAMACION ESTRUCTURADA 
+            //ejecucion de setencias secuenciales 
+            //uso de estructuras repetitivas
+            //uso de estructuras selectivas
+            saludos(nombre);
+            saludos("Jose Maria");
+            saludos("Pedro Pablo");
+
+            Console.WriteLine();
+            Console.WriteLine("Llamada a funcion que devuelve valor");
+            Console.WriteLine("Liquido a recibir para Juan {0}", calcularSueldo(300, 200));
+            Console.WriteLine("Liquido a recibir para Jose {0}", calcularSueldo(2000, 1000));
+            Console.WriteLine("Liquido a recibir para Pedro {0}", calcularSueldo(1340, 199));
+            Console.WriteLine("Liquido a recibir para Carlos {0}", calcularSueldo(8000, 5000));
             Console.ReadKey();
         }
+        //funciones: subprocesos - subprogramas - subrutinas
+        //dividir el codigo en modulos
+        //reutilizar el codigo 
+        //tipo: devuelven valores - no devuelven valores
+        static void saludos(String nombre) //parametro - argumento 
+        {
+
+            Console.WriteLine("Funcion que no devuelve valores...");
+            Console.WriteLine("HOLA {0} ", nombre);
+        }
+
+        //funcion que devuelve valor
+        //la funcion devuelve un tipo de dato
+       static int calcularSueldo(int ingreso, int egreso)
+        {
+
+            return (ingreso-egreso);
+        }
+
     }
+
 }
